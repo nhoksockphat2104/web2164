d755ef1a4a5427ab69c7e21465aefcf81249c925
nhoksockphat2104 <84694995+nhoksockphat2104@users.noreply.github.com>
2021-06-04 10:36:41 +0700

    Update 02.03.01_index.js

--- 02.03.01_index.js
+++ 02.03.01_index.js
@@ -1,38 +1,50 @@
-// Tạo projet và ghi nhận thông tin liên quan
-// npm init--y
-// Mở thư viện
-const fs = require ("fs");
+// Tạo Project và Ghi nhận thông tin liên quan
+// npm init --y
+// npm install --save express
+// Mở thư viện fs, express
+// const fs = require("fs");
 const express = require("express");
-// Tham số của Web
+var path = require('path');
+// Tham số - Web lắng nghe tại xport
 xport = process.env.PORT || 3000;
 // Tạo App Web
 const app = express();
 const router = express.Router();
-//Routing Web
+// Routing WEB
+// MVC
 
 //--- Home
-router.get('/', 
+router.get('/',
     (req, res) => {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.send("Đây là trang Home !");
-    });
+        res.sendFile(__dirname + "/View/home.html");
+    }
+);
+
+//--- Profile
+router.get('/profile',
+    (req, res) => {
+        res.sendFile(__dirname + "/View/profile.html");
+    }
+);
+
 //--- Login
-router.get('/Login', 
+router.get('/login',
     (req, res) => {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.send("Đây là trang Login !");
-    });
-//-- Log out
-router.get('/Logout', 
+        res.sendFile(__dirname + "/View/login.html");
+    }
+);
+
+//--- Logout
+router.get('/logout',
     (req, res) => {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.send("Đây là trang Log out !");
-    });
-// Start Web
+        res.sendFile(__dirname + "/View/logout.html");
+    }
+);
+
+// START WEB
 app.use("/", router);
-//Web lắng nghe tại port
 app.listen(xport,
-    (err) =>{
-        console.log("\n Tạo Web tại" ,xport ,"với", err);
+    (err) => {
+        console.log("\n Tạo Web tại ", xport, " với ", err);
     }
 );
